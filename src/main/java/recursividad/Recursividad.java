297ca082c92b9fde4820435831e5555a6939ab09
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-01-10 01:28:42 +0100

    Probando cosas dentro de cosas que ejecutan cosas

--- src/main/java/recursividad/Recursividad.java
+++ src/main/java/recursividad/Recursividad.java
@@ -1,47 +1,20 @@
 package recursividad;
 
-/*
- * Ejercicio 1. Crea un programa principal que muestre un menú de opciones y permita ejecutar desde ese mismo programa cualquiera de los ejercicios que realices a continuación. Por ejemplo:
- * Una vez ejecutado el ejercicio deseado, debemos preguntar siempre al usuario si quiere volver al menú principal o salir del programa. Por ejemplo:
+/* 
+* Proyecto-2 
+*/
 
-*Ejercicio 1. 
--Crea un método que obtenga la cantidad de dígitos de un número N mayor que cero. Se debe pasar como parámetro el número N. 
-⚠️OJO: no vale pasarlo a String y calcular el tamaño con la función length()!!!
-Si ese número N es 0, devuelvo 0.
-Si N no es 0, recorremos del número dividiéndolo entre 10 hasta llegar a 0. 
-Cada vez que dividamos entre 10, aumentamos en 1 nuestro contador.
-
-*Ejercicio 2. 
--Crea un método que obtenga el resultado de elevar un número a otro. Ambos números se deben pasar como parámetros. Los números deben ser positivos. 
-
-*Ejercicio 3. 
--Crea un método que dado un número positivo, lo imprima invertido por pantalla. 
-
--Haz lo mismo pero con una cadena de texto como entrada. Usa el método .toCharArray().
-
--En el programa principal de este ejercicio se deberá preguntar al usuario si quiere invertir una cadena o un número, y llamar al método que corresponda.
-
-*Ejercicio 4. 
--Crea un método que compruebe si un número es binario. Un número binario está formado únicamente por ceros y unos. 
-
-*Ejercicio 5. 
--Crea un método que obtenga el número binario de un número N pasado como parámetro. 
-
-*Ejercicio 6. 
--Crea un método que compruebe si una palabra está ordenada alfabéticamente. 
-
-*Ejercicio 7. 
--Crea un método que obtenga la suma de los números naturales desde 1 hasta N. Se debe pasar como parámetro el número N, debe ser mayor que cero. Se debe imprimir toda la cadena por consola. 
--Por ejemplo, para N=4 → ( 1+2+3+4 = 10).
-
-*/ 
+import java.util.Scanner;
 
 public class Recursividad {
 
-    //private static final Scanner entradaTeclado = new Scanner(System.in);
+    private static final Scanner entradaTeclado = new Scanner(System.in);
 
-    public static void main(String[] args) {
-
+     // Método que imprime el menú de opciones.
+    public static void imprimirMenuUno() {
+        System.out.println();
+        System.out.println("Selecciona el modo que quieres ejecutar:");
+        System.out.println();
         System.out.println("1 - Dígitos");
         System.out.println("2 - Potencias");
         System.out.println("3 - Del Revés");
@@ -49,41 +22,92 @@ public class Recursividad {
         System.out.println("5 - A binario");
         System.out.println("6 - Orden alfabético");
         System.out.println("7 - Mostrar suma");
-        System.out.print("Introduce una opción: ");
+        System.out.print("-> ");
     }
 
-    //Método para obtener la cantidad de dígitos de un número N mayor que cero.
+    // Método para imprimir el menú secundario.
+    public static void imprimirMenuDos() {
+        System.out.println();
+        System.out.println("Elige una opción: ");
+        System.out.println("   [M] - Volver al menú principal");
+        System.out.println("   [X] - Salir");
+        System.out.print("-> ");
+    }
+
+    // Método para leer la opción del usuario.
+    public static int leerOpcion() {
+        while (!entradaTeclado.hasNextInt()) {
+            System.out.println();
+            System.out.println("Error: Introduce un número válido:");
+            entradaTeclado.next();
+            imprimirMenuUno();
+        }
+        return entradaTeclado.nextInt();
+    }
+
+    // Método para leer la respuesta del usuario.
+    public static String leerRespuesta() {
+        return entradaTeclado.next().trim().toUpperCase();
+    }
+
+    // Método que llama al método correspondiente según la opción elegida.
+    public static void opcionElegida(int opcion) {
+        switch (opcion) {
+            case 1 -> cantidadDigitos();
+            case 2 -> elevarNumeroAOtro();
+            case 3 -> numeroInvertido();
+            case 4 -> comprobarNumeroBinario();
+            case 5 -> obtenerNumeroBinario();
+            case 6 -> comprobarPalabraOrdenada();
+            case 7 -> sumaNumerosNaturales();
+        }
+    }
+
+    // Método para obtener la cantidad de dígitos de un número N mayor que cero.
     public static void cantidadDigitos() {
+        System.out.println();
+        System.out.print("Introduce un número: ");
+        int numero = entradaTeclado.nextInt();
+        int contador = 0;
 
+        if (numero == 0) {
+            System.out.println("El resultado es: 0");
+        } else {
+            while (numero != 0) {
+                numero = numero / 10;
+                contador++;
+            }
+            System.out.println("El resultado es: " + contador);
+        }
     }
 
-    //Método que obtener el resultado de elevar un número a otro.
-    public static void elevarNumeroAOtro(){
-
+    // Método que obtiene el resultado de elevar un número a otro.
+    public static void elevarNumeroAOtro() {
+        
     }
 
-    //Método para invertir un número positivo.
-    public static void numeroInvertido(){
-
+    // Método para invertir un número positivo.
+    public static void numeroInvertido() {
+        
     }
 
-    //Método para comprobar si un número es binario.
-    public static void comprobarNumeroBinario(){
-
+    // Método para comprobar si un número es binario.
+    public static void comprobarNumeroBinario() {
+        
     }
 
-    //Método que obtenga el número binario de un número N pasado como parámetro. 
-    public static void obtenerNumeroBinario(){
-
+    // Método que obtiene el número binario de un número N pasado como parámetro.
+    public static void obtenerNumeroBinario() {
+        
     }
 
-    //étodo que compruebe si una palabra está ordenada alfabéticamente.
-    public static void comprobarPalabraOrdenada(){
-
+    // Método que comprueba si una palabra está ordenada alfabéticamente.
+    public static void comprobarPalabraOrdenada() {
+        
     }
 
-    //Método que obtenga la suma de los números naturales desde 1 hasta N.
-    public static void sumaNumerosNaturales(){
-
+    // Método que obtiene la suma de los números naturales desde 1 hasta N.
+    public static void sumaNumerosNaturales() {
+        
     }
 }
